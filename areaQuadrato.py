4687fe7dbff2935d457be50150d165c487931737
lucadar <50534610+lucadar@users.noreply.github.com>
2020-05-09 10:25:19 +0200

    Creazione areaQuadrato.py
    ho riutilizzato il codice areaTriangolo.py

---
+++ areaQuadrato.py
@@ -0,0 +1,7 @@
+# prog_Python
+Primi esempi di programmi Python
+# calcolo area del quadrato
+print("Inserisci il lato del quadrato")
+lato=int(input())
+area=lato*lato
+print("Il quadrato ha un'area di: ", area)
