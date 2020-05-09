f17685222d139f152c98894c5d4880e01dc72fad
lucadar <50534610+lucadar@users.noreply.github.com>
2020-05-09 10:16:47 +0200

    Create areaTriangolo.py
    Ho riutilizzato il codice di areaRettangolo

---
+++ areaTriangolo.py
@@ -0,0 +1,9 @@
+# prog_Python
+Primi esempi di programmi Python
+# calcolo area del triangolo
+print("Inserisci la base del rettangolo")
+base=int(input())
+print("Inserisci l'altezza")
+altezza= int(input())
+area=base*altezza/2
+print("Il triangolo ha un'area di: ", area)
